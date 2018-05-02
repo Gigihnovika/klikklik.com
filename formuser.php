f72b9464892fb2574bcbe14998cc37759ccb92e8
Gigihnovika <gigihvika24@gmail.com>
2018-05-02 12:38:01 +0800

    first commit

---
+++ formuser.php
@@ -0,0 +1,17 @@
+<<!DOCTYPE html>
+<html>
+<head>
+<title>Selamat Datang di Klikklik.com</title>
+</head>
+<body>
+    <table>
+        <form method="GET">
+            <tr>
+                <td><input type="text" name ="name"></td>
+                <td><input type="password" name ="password"></td>
+                <td><button>Login</button></td>
+</tr>
+</form>
+</table>
+</body>
+</html>
